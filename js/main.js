013b4ea6cc04ba17a4a5c17040d33b169f3cb402
Sozark <121688154+Sozark@users.noreply.github.com>
2024-10-30 18:07:03 -0500

    Attempt at adding multiple videos

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
-// JS scripts placed here
+var myvid = document.getElementByClass('myvideo');
+var myvids = [
+  "https://pixabay.com/videos/fire-flame-heat-light-up-9164/", 
+  "https://pixabay.com/videos/flames-heat-warm-warmth-burn-oven-2336/"
+  ];
+var activeVideo = 0;
+
+myvid.addEventListener('ended', function(e) {
+  // update the new active video index
+  activeVideo = (++activeVideo) % myvids.length;
+
+  // update the video source and play
+  myvid.src = myvids[activeVideo];
+  myvid.play();
+});
+
+
