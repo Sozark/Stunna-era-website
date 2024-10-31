33c2ae7602c547a01c0fe343063a5eb0e67ed306
Sozark <121688154+Sozark@users.noreply.github.com>
2024-10-30 22:31:26 -0500

    Re positioned artists and removed Future for now

--- js/main.js
+++ js/main.js
@@ -1,17 +1,2 @@
-var myvid = document.getElementByClass('myvideo');
-var myvids = [
-  "https://pixabay.com/videos/fire-flame-heat-light-up-9164/", 
-  "https://pixabay.com/videos/flames-heat-warm-warmth-burn-oven-2336/"
-  ];
-var activeVideo = 0;
-
-myvid.addEventListener('ended', function(e) {
-  // update the new active video index
-  activeVideo = (++activeVideo) % myvids.length;
-
-  // update the video source and play
-  myvid.src = myvids[activeVideo];
-  myvid.play();
-});
 
 
